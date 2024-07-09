863d16da26ae4542c8de93cbb4ab22219f92f8b3
Shiva Krishna D <shiva@xcontrol.de>
2024-07-09 15:45:55 +0200

    Add fonts, material and bootstrap

---
+++ src/app/modules/bootstrap.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import {
+  NgbAccordionModule,
+  NgbAlertModule,
+  NgbDropdownModule,
+  NgbDatepickerModule,
+  NgbNavModule,
+  NgbPopoverModule,
+  NgbTimepickerModule,
+  NgbToastModule,
+  NgbTooltipModule,
+  NgbModalModule,
+} from '@ng-bootstrap/ng-bootstrap';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    NgbAccordionModule,
+    NgbAlertModule,
+    NgbDropdownModule,
+    NgbDatepickerModule,
+    NgbModalModule,
+    NgbNavModule,
+    NgbPopoverModule,
+    NgbTimepickerModule,
+    NgbToastModule,
+    NgbTooltipModule,
+  ],
+})
+export class BootstrapModule {}
